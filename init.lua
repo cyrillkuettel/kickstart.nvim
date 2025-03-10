62d8b059c891353504879e4ec2867f98365baf3c
Cyrill Küttel <cyrillkuettel@gmail.com>
2025-03-10 03:50:02 +0100

    paste without replacing clipboard

--- init.lua
+++ init.lua
@@ -193,6 +193,8 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+vim.keymap.set('x', 'p', 'P', { desc = 'paste without replacing clipboard' })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
