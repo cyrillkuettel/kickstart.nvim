c91a045e49979580a6f1ae5d7e9dbff674891449
Cyrill Küttel <cyrillkuettel@gmail.com>
2025-03-13 12:01:34 +0100

    Add basedpyright, disable all diagnostics for now.

--- init.lua
+++ init.lua
@@ -405,6 +405,7 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
       vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
@@ -656,8 +657,28 @@ require('lazy').setup({
             disableOrganizeImports = true,
             basedpyright = {
               analysis = {
-                -- ignore = { "*" },
-                typeCheckingMode = 'standard',
+                diagnosticSeverityOverrides = {
+
+                  -- This completely disables all error diagnostics
+                  reportGeneralTypeIssues = 'none',
+                  reportOptionalMemberAccess = 'none',
+                  reportOptionalSubscript = 'none',
+                  reportPrivateImportUsage = 'none',
+                  reportUnboundVariable = 'none',
+                  reportUnusedImport = 'none',
+                  reportMissingImports = 'none',
+                  reportInvalidTypeVarUse = 'none',
+                  reportMissingTypeStubs = 'none',
+                  reportUndefinedVariable = 'none',
+                  reportUnusedVariable = 'none',
+                  reportUnusedFunction = 'none',
+                  reportUnusedClass = 'none',
+                  reportOptionalIterable = 'none',
+                  reportOptionalContextManager = 'none',
+                  reportOptionalOperand = 'none',
+                  -- Add any other error types you want to disable
+                },
+                typeCheckingMode = 'off', -- Change to 'off' instead of 'standard'
                 diagnosticMode = 'openFilesOnly',
                 useLibraryCodeForTypes = true,
               },
