361dbe68effbec470972a17df87405a417ae1cb6
Cyrill Küttel <cyrillkuettel@gmail.com>
2025-03-13 13:37:01 +0100

    Add nerdtree toggle.
    Shift tab may not be optimal, but leave as that for now.

--- init.lua
+++ init.lua
@@ -169,6 +169,8 @@ vim.opt.scrolloff = 10
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
+vim.keymap.set('n', '<S-Tab>', ':Neotree toggle<CR>', { noremap = true, silent = true })
+
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
