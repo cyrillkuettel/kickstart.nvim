f9c2766b79d59de3726856e9d5f35a9f6117168c
Cyrill Küttel <cyrillkuettel@gmail.com>
2025-03-14 13:10:31 +0100

    double Esc to return to normal mode from term mode

--- lua/custom/plugins/fterm.lua
+++ lua/custom/plugins/fterm.lua
@@ -7,14 +7,17 @@ return {
     fterm.setup {
       border = 'rounded',
       dimensions = {
-        height = 0.8,
-        width = 0.8,
+        height = 0.8, -- Height of the terminal window
+        width = 0.8, -- Width of the terminal window
+        x = 0.5, -- X axis of the terminal window
+        y = 0.5, -- Y axis of the terminal window
       },
     }
 
     -- F4 keybinding for toggling terminal in normal mode
     vim.keymap.set('n', '<F4>', '<CMD>lua require("FTerm").toggle()<CR>')
-    -- F4 keybinding for toggling terminal in terminal mode
-    vim.keymap.set('t', '<F4>', '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>')
+
+    -- double Esc for ability to treat the terminal like vim buffer
+    vim.keymap.set('t', '<esc><esc>', [[<C-\><C-n>]], { silent = true, noremap = true })
   end,
 }
