107ffbecd387b0d915c7bf2eb8848e243284cc75
Cyrill Küttel <cyrillkuettel@gmail.com>
2025-03-20 21:36:27 +0100

    Revert "Highlight uses of word under cursor (local-highlight.nvim)"
    This reverts commit 7a66eecf84e3c72762b1024cc468028d9b912dca.

--- init.lua
+++ init.lua
@@ -376,34 +376,6 @@ require('lazy').setup({
     lazy = false,
   },
   {
-    -- Using regexes and extmarks to highlight uses of the word under the
-    -- cursor. Keeps updates local to currently visible lines, thus enabling blazingly fast performance.
-    'tzachar/local-highlight.nvim',
-    config = function()
-      require('local-highlight').setup {
-        file_types = { 'python', 'cpp' }, -- If this is given only attach to this
-        -- OR attach to every filetype except:
-        disable_file_types = { 'tex' },
-        hlgroup = 'LocalHighlight',
-        cw_hlgroup = nil,
-        -- Whether to display highlights in INSERT mode or not
-        insert_mode = false,
-        min_match_len = 1,
-        max_match_len = math.huge,
-        highlight_single_match = true,
-        animate = {
-          enabled = false, -- Requries snacks.nvim, leave for now
-          easing = 'linear',
-          duration = {
-            step = 10, -- ms per step
-            total = 100, -- maximum duration
-          },
-        },
-        debounce_timeout = 200,
-      }
-    end,
-  },
-  {
     'nvim-neo-tree/neo-tree.nvim',
     branch = 'v3.x',
     dependencies = {
