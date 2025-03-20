0f739453a961864065034e9fd6e894896fd88544
Cyrill Küttel <cyrillkuettel@gmail.com>
2025-03-20 03:54:15 +0100

    Scroll with more speeed

--- lua/custom/plugins/neoscroll.lua
+++ lua/custom/plugins/neoscroll.lua
@@ -17,8 +17,8 @@ return {
       stop_eof = true, -- Stop at <EOF> when scrolling downwards
       respect_scrolloff = false, -- Stop scrolling when the cursor reaches the scrolloff margin of the file
       cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
-      duration_multiplier = 1.2, -- Global duration multiplier
-      easing = 'linear', -- (linear, quadratic, cubic, quartic, quintic, circular, sin)
+      duration_multiplier = 0.6, -- Global duration multiplier
+      easing = 'sin', -- (linear, quadratic, cubic, quartic, quintic, circular, sin)
       pre_hook = nil, -- Function to run before the scrolling animation starts
       post_hook = nil, -- Function to run after the scrolling animation ends
       performance_mode = false, -- Disable "Performance Mode" on all buffers.
