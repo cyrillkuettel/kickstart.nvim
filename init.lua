d90d4a7f72f8759a4711f92e38c51b5e78c11911
Cyrill Küttel <cyrillkuettel@gmail.com>
2025-04-02 23:48:13 +0200

    Switch colorscheme shortcut telescope with live-preview

--- init.lua
+++ init.lua
@@ -243,6 +243,13 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 -- or just use <C-\><C-n> to exit terminal mode
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
+-- colorscheme switching shortcut [cyrill]
+vim.api.nvim_create_user_command('ColorSchemeSwitch', function()
+  require('telescope.builtin').colorscheme()
+end, { desc = 'Switch Colorscheme' })
+
+vim.keymap.set('n', '<leader>cs', ':ColorSchemeSwitch<CR>', { desc = 'Switch Colorscheme' })
+
 -- TIP: Disable arrow keys in normal mode
 -- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
 -- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
