608a511cc99219b5cd1a747ee8fa719a1550e1c6
Cyrill Küttel <cyrillkuettel@gmail.com>
2025-04-06 14:50:33 +0200

    blame.lua: make colors work again
    Colors were broken because of the format function

--- lua/custom/plugins/blame.lua
+++ lua/custom/plugins/blame.lua
@@ -3,44 +3,59 @@ return {
     'FabijanZulj/blame.nvim',
     lazy = false,
     config = function()
-      local blame = require('blame')
-      
+      local blame = require 'blame'
+
       -- Custom format function with first name only, no revision
       local function first_name_date_fn(line_porcelain, config, idx)
         -- Extract first name only
         local first_name = string.match(line_porcelain.author, '^(%S+)')
         local date = os.date(config.date_format, line_porcelain.author_time)
-        
+
+        -- Make sure we have a valid hash to create highlight group
+        local hash = line_porcelain.hash or ''
+        if hash == '' then
+          hash = 'unknown'
+        end
+
+        -- Use the proper highlight group naming used by the plugin
+        local hl_group = 'Blame' .. hash
+
         return {
           idx = idx,
           values = {
-            { textValue = date, hl = 'Blame' .. line_porcelain.hash },
-            { textValue = first_name, hl = 'Blame' .. line_porcelain.hash }
+            { textValue = date, hl = hl_group },
+            { textValue = ' (' .. first_name .. ')', hl = hl_group },
           },
-          format = '%s (%s)'  -- Format: date (first_name)
+          format = '%s%s', -- Format: date (first_name)
         }
       end
-      
-      -- Define highlight groups with different intensities
-      vim.api.nvim_command('highlight BlameRecent guifg=#50fa7b gui=bold')
-      vim.api.nvim_command('highlight BlameDaysOld guifg=#8be9fd')
-      vim.api.nvim_command('highlight BlameWeeksOld guifg=#bd93f9')
-      vim.api.nvim_command('highlight BlameMonthsOld guifg=#ff79c6')
-      vim.api.nvim_command('highlight BlameOld guifg=#6272a4')
-      
-      blame.setup({
+
+      -- Set explicit colors to ensure they're visible
+      local explicit_colors = {
+        '#ff6188', -- red
+        '#a9dc76', -- green
+        '#78dce8', -- blue
+        '#ffd866', -- yellow
+        '#ab9df2', -- purple
+        '#fc9867', -- orange
+      }
+      blame.setup {
         date_format = '%d.%m.%Y',
-        virtual_style = 'float',
+        virtual_style = 'right_align',
         focus_blame = true,
         merge_consecutive = false,
         max_summary_width = 30,
-        colors = nil,
+        colors = explicit_colors,
         blame_options = nil,
         commit_detail_view = 'vsplit',
-        format_fn = first_name_date_fn
-      })
+        mappings = {
+          commit_info = 'i',
+          stack_push = '<TAB>',
+          stack_pop = '<BS>',
+          show_commit = '<CR>',
+          close = { '<esc>', 'q' },
+        },
+      }
     end,
   },
-  vim.keymap.set('n', '<leader>g', ':BlameToggle virtual<CR>', 
-                 { noremap = true, silent = true }),
 }
