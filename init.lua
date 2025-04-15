31e38042fa7a8a1c5ebf7872869d923306f2aba1
Cyrill Küttel <cyrillkuettel@gmail.com>
2025-04-15 15:56:25 +0200

    fix: Remove duplicate neo-tree plugin definition in init.lua

--- init.lua
+++ init.lua
@@ -400,16 +400,6 @@ require('lazy').setup({
     'eandrju/cellular-automaton.nvim',
     lazy = false,
   },
-  {
-    'nvim-neo-tree/neo-tree.nvim',
-    branch = 'v3.x',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
-      'MunifTanjim/nui.nvim',
-      -- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
-    },
-  },
 
   { -- Buffer Line (Tabs) [cyrill]
     'akinsho/bufferline.nvim',
