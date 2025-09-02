a6788f6412e0501978ce6a0a282de2a0afb73568
cyrillkuettel <cyrillkuettel@gmail.com>
2025-09-02 04:53:34 +0200

    Make diagnostics italics (looks nice with the right font)

--- lua/custom/plugins/tiny-line-diagnostic.lua
+++ lua/custom/plugins/tiny-line-diagnostic.lua
@@ -3,7 +3,17 @@ return {
   event = 'VeryLazy', -- Or `LspAttach`
   priority = 1000, -- needs to be loaded in first
   config = function()
-    require('tiny-inline-diagnostic').setup()
+    require('tiny-inline-diagnostic').setup({
+      options = {
+        format = function(diagnostic)
+          return diagnostic.message
+        end,
+      },
+    })
     vim.diagnostic.config { virtual_text = false } -- Only if needed in your configuration, if you already have native LSP diagnostics
+    vim.cmd 'highlight TinyInlineDiagnosticVirtualTextError gui=italic'
+    vim.cmd 'highlight TinyInlineDiagnosticVirtualTextWarn gui=italic'
+    vim.cmd 'highlight TinyInlineDiagnosticVirtualTextInfo gui=italic'
+    vim.cmd 'highlight TinyInlineDiagnosticVirtualTextHint gui=italic'
   end,
 }
