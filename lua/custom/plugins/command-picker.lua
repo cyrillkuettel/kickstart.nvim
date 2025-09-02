8cd5879363cfb6b921166a82143b1f9abd50e2ba
cyrillkuettel <cyrillkuettel@gmail.com>
2025-09-02 05:53:33 +0200

    feat: add command-picker plugin

---
+++ lua/custom/plugins/command-picker.lua
@@ -0,0 +1,63 @@
+return {
+  'nvim-telescope/telescope.nvim',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  opts = function()
+    local actions = require 'telescope.actions'
+    local action_state = require 'telescope.actions.state'
+    local pickers = require 'telescope.pickers'
+    local finders = require 'telescope.finders'
+    local conf = require('telescope.config').values
+
+    -- Define commonly used commands
+    local commands = {
+      {
+        name = 'Get Current Working Directory',
+        command = function()
+          print(vim.fn.getcwd())
+        end,
+      },
+      { name = 'LSP Restart', command = ':LspRestart' },
+      { name = 'Telescope Builtin', command = ':Telescope builtin' },
+      { name = 'Git Status', command = ':Telescope git_status' },
+      { name = 'Checkhealth', command = ':checkhealth' },
+    }
+
+    local command_picker = function(opts)
+      opts = opts or {}
+      pickers
+        .new(opts, {
+          prompt_title = "Cyrill's Common Commands",
+          finder = finders.new_table {
+            results = commands,
+            entry_maker = function(entry)
+              return {
+                value = entry,
+                display = entry.name,
+                ordinal = entry.name,
+              }
+            end,
+          },
+          sorter = conf.generic_sorter(opts),
+          attach_mappings = function(prompt_bufnr, map)
+            actions.select_default:replace(function()
+              actions.close(prompt_bufnr)
+              local selection = action_state.get_selected_entry()
+              if selection and selection.value then
+                local cmd = selection.value.command
+                if type(cmd) == 'function' then
+                  cmd()
+                else
+                  vim.cmd(cmd)
+                end
+              end
+            end)
+            return true
+          end,
+        })
+        :find()
+    end
+
+    -- Create the keymap
+    vim.keymap.set('n', '<leader>mcc', command_picker, { desc = 'My Common Commands Picker' })
+  end,
+}
